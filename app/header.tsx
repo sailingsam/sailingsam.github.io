876dd10fba39c787262108d7af337250f1114636
Saksham <jainsaksham1004@gmail.com>
2025-08-17 13:42:17 +0530

    added avatar

--- app/header.tsx
+++ app/header.tsx
@@ -1,23 +1,33 @@
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export function Header() {
   return (
     <header className="mb-8 flex items-center justify-between">
-      <div>
-        <Link href="/" className="font-medium text-black dark:text-white">
-          Saksham Jain
-        </Link>
-        <TextEffect
-          as="p"
-          preset="fade"
-          per="char"
-          className="text-zinc-600 dark:text-zinc-500"
-          delay={0.5}
-        >
-          Software Engineer
-        </TextEffect>
+      <div className="flex items-center gap-4">
+        <Image
+          src="/avatar.webp"
+          alt="Saksham Jain"
+          width={60}
+          height={60}
+          className="rounded-full"
+        />
+        <div>
+          <Link href="/" className="font-medium text-black dark:text-white">
+            Saksham Jain
+          </Link>
+          <TextEffect
+            as="p"
+            preset="fade"
+            per="char"
+            className="text-zinc-600 dark:text-zinc-500"
+            delay={0.5}
+          >
+            Software Engineer
+          </TextEffect>
+        </div>
       </div>
     </header>
   )
